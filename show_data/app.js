40c79289210629f56206c5fd7614e3b25a1487df
app <app@app.com>
2019-11-03 15:19:11 -0800

    automatic column names

--- show_data/app.js
+++ show_data/app.js
@@ -3,10 +3,10 @@ var con = require("../db_con").con;
 
 var app=express();
 
+//api to fetch data from database
 app.get('/api/v1/get_data',function(req,res)
 {
   var data = {};
-
   con.query("select * from users;select * from dates;", function (err, result) {
     if (err) throw err;
     data['users'] = result[0];
@@ -15,9 +15,11 @@ app.get('/api/v1/get_data',function(req,res)
   });
 });
 
+//html page route to show data
 app.get('/',function(req,res)
 {
   res.sendFile(__dirname+'/index.html');
 });
 
+//hosted on port 8080
 var server=app.listen(8080,function() {});
